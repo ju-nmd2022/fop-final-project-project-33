e67ef1597c2bcc5af8808a9558f073dd78762f38
CosminOstafie <116910376+CosminOstafie@users.noreply.github.com>
2023-04-26 13:56:08 +0200

    Background image added + work on the submarine

--- game.js
+++ game.js
@@ -8,7 +8,6 @@ function setup() {
   createCanvas(800, 600);
 }
 
-
 let myImage;
 
 function preload() {
@@ -19,7 +18,29 @@ function setup() {
   createCanvas(800, 600);
 }
 
+//bezierCurves -> vertex(x1,y1)
+//      bezierVertex(x2,y2)
+
 function draw() {
   background(255);
   image(myImage, 0, 0, 800, 600);
+  //Draw the body of the submarine
+  noStroke();
+  fill(100, 150, 200);
+  beginShape();
+  vertex(100, 200);
+  bezierVertex(120, 100, 280, 140, 380, 200);
+  bezierVertex(300, 260, 120, 300, 100, 200);
+  endShape(CLOSE);
+  //Draw the windows of the submarine
+  push();
+  fill(33);
+  ellipse(145, 200, 25);
+  ellipse(195, 200, 25);
+  ellipse(245, 200, 25);
+  ellipse(295, 200, 25);
+  pop();
+  //rect(x,y,w,[h],[topleft],[topright],[bottomright],[bottomleft])
+  fill(33);
+  rect(165, 95, 65, 50, 20, 59, 0);
 }
