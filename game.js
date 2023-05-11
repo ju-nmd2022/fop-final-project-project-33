56b09ce5d811fd2d21931a3d10bf7869e594e5cf
AlaaAlkhaled <alaa.alkhaled7@gmail.com>
2023-05-11 15:23:51 +0200

    update
    changing the structure of some functions

--- game.js
+++ game.js
@@ -310,8 +310,8 @@ function drawToxicBarrel(x, y, scaleFactor) {
 
   // the rounded top
   fill(255, 255, 0);
-  arc(x, y - 59 * 0.6, barrelWidth, curveAmount, PI, 3, OPEN); // Reduced y-coordinate
-  arc(x, y + 59 * 0.6, barrelWidth, curveAmount, PI, 3, OPEN); // Reduced y-coordinate
+  arc(x, y - 59 * 0.6, barrelWidth, curveAmount, PI, 3, OPEN);
+  arc(x, y + 59 * 0.6, barrelWidth, curveAmount, PI, 3, OPEN); 
 
   // the toxic symbol
   fill(0);
@@ -357,15 +357,18 @@ function draw() {
     pop();
   }
 
-  // underwater mines
-  for (let mine of mines) {
+// underwater mines
+for (let mine of mines) {
+  let mineX = mine.x / mine.scaleFactor + 20 * sin(frameCount * 0.02 + mine.offsetX);
+  let mineY = mine.y / mine.scaleFactor;
+  
+  if (mineX > 300 && mineY > 300) {
     push();
     scale(mine.scaleFactor);
-    let mineX =
-      mine.x / mine.scaleFactor + 20 * sin(frameCount * 0.02 + mine.offsetX);
-    UnderwaterMine(mineX, mine.y / mine.scaleFactor);
+    UnderwaterMine(mineX, mineY);
     pop();
   }
+}
 
   // the submarine
   if (submarineVisible) {
