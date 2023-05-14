691f7d347f130d6b3685a13b42f65b190159369c
CosminOstafie <116910376+CosminOstafie@users.noreply.github.com>
2023-05-14 23:54:30 +0200

    Added movement to the submarine + image

--- game2.js
+++ game2.js
@@ -1,7 +1,7 @@
 //Importing the necessary classes
-import Submarine from "./submarine";
-import Mine from "./mine";
-import ToxicBarrel from "./ToxicBarrel";
+import SubmarineClass from "./submarine.js";
+// import Mine from "./mine";
+// import ToxicBarrel from "./ToxicBarrel";
 
 // Game State constants for the switch statement
 const STATE_START = 0;
@@ -10,17 +10,22 @@ const STATE_GAME_OVER = 2;
 
 let gameState; // Variable used to switch and compare the states of the game
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+let submarineImage;
+let submarine;
 
 function preload() {
-  myImage = loadImage("/img/background2.png");
+  backgroundImage = loadImage("/img/background2.png");
+  submarineImage = loadImage("/img/submarine-graphic.png");
+}
+function setup() {
+  let cnv = createCanvas(windowWidth, windowHeight);
+  submarine = new SubmarineClass(width / 2, height / 2, 4, submarineImage);
 }
 
 function draw() {
   background(255);
-  image(myImage, 0, 0, windowWidth, windowHeight);
-  let submarine = new Submarine();
+  image(backgroundImage, 0, 0, windowWidth, windowHeight);
+
   submarine.display();
+  submarine.move();
 }
